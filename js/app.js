3cad22a860047cb800491c4916ef3e78fe8b40ee
mrmyers5280 <not.that.mike.myers@gmail.com>
2015-07-28 14:35:08 -0600

    With app.js, add click behavior to New Game button

--- js/app.js
+++ js/app.js
@@ -5,12 +5,15 @@ $(document).ready(function(){
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
   	});
-
   	/*--- Hide information modal box ---*/
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+	$('nav ul').on('click', '.new', function() {
+		logIt("New Game button clicked.")
+		// newGame(randomNum());
+	});
 	function randomNum() {
 		num = Math.round((Math.random() * 100) + 1);
 		logIt("num is: " + num);
