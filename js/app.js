43ae76dff7fcefc7f60cfec2672f215cc3b56afc
mrmyers5280 <not.that.mike.myers@gmail.com>
2015-07-29 15:51:42 -0600

    With app.js, add if else logic to determine how close user guess is

--- js/app.js
+++ js/app.js
@@ -26,10 +26,28 @@ $(document).ready(function(){
     }
     $('#guessButton').click(function() {
         logIt("Guess button clicked.");
-        guessNum = parseInt($("#userGuess").val(), 10);
+        guessNum = parseInt($('#userGuess').val(), 10);
         logIt("guessNum is: " + guessNum);
         // howClose(randNum, guessNum);
         // testing logic goes here.
+        var guessDiff = Math.abs(guessNum - randNum);   // find the difference
+        logIt("guessDiff is: " + guessDiff);
+        if (guessDiff > 100) {
+            alert("Your number is too large. Please pick a number between 1 and 100, including 1 and 100.")
+        } else if ((guessDiff <= 100) && (guessDiff > 50)) {
+            logIt("ice cold");
+        } else if ((guessDiff <= 50) && (guessDiff > 30)) {
+            logIt("cold");
+        } else if ((guessDiff <= 30) && (guessDiff > 20)) {
+            logIt("warm");
+        } else if ((guessDiff <= 20) && (guessDiff > 10)) {
+            logIt("hot");
+        } else if ((guessDiff <= 10) && (guessDiff >= 1)) {
+            logIt("very hot");
+        } else if (guessDiff === 0) {
+            logIt("Congratulations, you got it!");
+        }
+
         // add number to user guessList.
     });
     function logIt(message) {
