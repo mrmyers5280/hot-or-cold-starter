4712c67a5f356c89373e9aae36f4136231a14ab9
mrmyers5280 <not.that.mike.myers@gmail.com>
2015-07-29 15:24:38 -0600

    With index.html and app.js, convert tabs to spaces with plugin

--- js/app.js
+++ js/app.js
@@ -2,36 +2,38 @@ $(document).ready(function(){
 
     var randNum, guessNum;
 
-	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
-  	});
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
+    /*--- Display information modal box ---*/
+    $(".what").click(function(){
+        $(".overlay").fadeIn(1000);
+    });
+    /*--- Hide information modal box ---*/
+    $("a.close").click(function(){
+         $(".overlay").fadeOut(1000);
+    });
 
-	$('a.new').click(function() {
-		logIt("New Game button clicked.")
-		newGame(randomNum());
-	});
-	function newGame(number) {
-		logIt("In newGame function.");
-		logIt("number is: " + number);
-	}
-	function randomNum() {
-		randNum = Math.floor((Math.random() * 100) + 1);
-		logIt("num is: " + randNum);
-		return randNum;
-	}
+    $('a.new').click(function() {
+        logIt("New Game button clicked.")
+        newGame(randomNum());
+    });
+    function newGame(number) {
+        logIt("In newGame function.");
+        logIt("number is: " + number);
+    }
+    function randomNum() {
+        randNum = Math.floor((Math.random() * 100) + 1);
+        logIt("num is: " + randNum);
+        return randNum;
+    }
     $('#guessButton').click(function() {
         logIt("Guess button clicked.");
         guessNum = parseInt($("#userGuess").val(), 10);
         logIt("guessNum is: " + guessNum);
         // howClose(randNum, guessNum);
+        // testing logic goes here.
+        // add number to user guessList.
     });
-	function logIt(message) {
-		console.log(message);
-	}
-	newGame(randomNum());
+    function logIt(message) {
+        console.log(message);
+    }
+    newGame(randomNum());
 });
