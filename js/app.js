2ac4466e46cee3041fa7e1620ad953de5229d81e
mrmyers5280 <not.that.mike.myers@gmail.com>
2015-08-04 16:34:55 -0600

    With app.js, refactor to reduce duplicated statements and jQuery calls

--- js/app.js
+++ js/app.js
@@ -8,26 +8,31 @@
 //THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 $(document).ready(function(){
 
-    var randNum, guessNum;
+    var randNum = 0 ;
+    var guessNum = 0;
     var guesses = 0;
+    var feedbackText = '';
+    var guessCount = $('#count');
     var feedback = $('#feedback');
     var guessList = $('#guessList');
+    var userGuess = $('#userGuess');
+    var guessButton = $('#guessButton');
 
     /*--- Display information modal box ---*/
-    $(".what").click(function(){
-        $(".overlay").fadeIn(1000);
+    $('.what').click(function(){
+        $('.overlay').fadeIn(1000);
     });
     /*--- Hide information modal box ---*/
-    $("a.close").click(function(){
-         $(".overlay").fadeOut(1000);
+    $('a.close').click(function(){
+         $('.overlay').fadeOut(1000);
     });
-    $('#guessButton').click(function() {
-        guessNum = parseInt($('#userGuess').val(), 10);
+    guessButton.click(function() {
+        guessNum = parseInt(userGuess.val(), 10);
         howClose();
         guesses++;
-        logIt("guesses is: " + guesses);
+        logIt('guesses is: ' + guesses);
         // show number of guesses
-        $('#count').text(guesses);
+        guessCount.text(guesses);
         // add guessed number to user guessList.
         guessList.append('<li>' + guessNum + '</li>');
     });
@@ -36,39 +41,40 @@ $(document).ready(function(){
     });
     function newGame(number) {
         // enable the Guess button, if it's disabled
-        if ($('#guessButton').attr('disabled') === 'disabled') {
-            $('#guessButton').removeAttr('disabled', '');
+        if (guessButton.attr('disabled') === 'disabled') {
+            guessButton.removeAttr('disabled', '');
         }
         // reset feedback
         feedback.text('Make your Guess!');
         // reset userGuess text field
-        $('#userGuess').val('');
+        userGuess.val('');
         // reset guessed numbers
         guessList.empty();
         // reset guesses count
         guesses = 0;
-        $('#count').text(guesses);
+        guessCount.text(guesses);
     }
     function howClose() {
         // testing logic goes here.
         var guessDiff = Math.abs(guessNum - randNum);   // find the difference
         if (guessDiff > 100) {
-            alert("Your number is too large. Please pick a number between 1 and 100, including 1 and 100.")
+            alert('Your number is too large. Please pick a number between 1 and 100, including 1 and 100.')
         } else if ((guessDiff <= 100) && (guessDiff > 50)) {
-            feedback.text('Ice cold!');
+            feedbackText = 'Ice cold!';
         } else if ((guessDiff <= 50) && (guessDiff > 30)) {
-            feedback.text('Cold');
+            feedbackText = 'Cold';
         } else if ((guessDiff <= 30) && (guessDiff > 15)) {
-            feedback.text('Warm');
+            feedbackText = 'Warm';
         } else if ((guessDiff <= 15) && (guessDiff > 5)) {
-            feedback.text('Hot!');
+            feedbackText = 'Hot!';
         } else if ((guessDiff <= 5) && (guessDiff >= 1)) {
-            feedback.text('Very hot!');
+            feedbackText = 'Very hot!';
         } else if (guessDiff === 0) {
-            feedback.text('Congratulations, you got it!');
-            $('#guessButton').attr('disabled', ''); // disable the Guess button
+            feedbackText = 'Congratulations, you got it!');
+            guessButton.attr('disabled', ''); // disable the Guess button
             alert('Great job! Click the New Game button to start over.');
         }
+        feedback.text(feedbackText);
     }
     function randomNum() {
         randNum = Math.floor((Math.random() * 100) + 1);
